2058d52e26fafdc152cec6d163928a10e418c2da
rakib <rhkhan9618@gmail.com>
2023-07-18 23:29:59 +0600

    working on design

---
+++ app.vue
@@ -0,0 +1,6 @@
+<template>
+  <div class="bg-[#353f5b] text-white min-h-screen">
+    <!-- <NuxtWelcome /> -->
+    <NuxtPage />
+  </div>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="max-w-[400px] mx-auto">
+    <h1 class="text-center text-4xl font-medium py-10">Todo List</h1>
+    <div class="cls">
+      <h3 v-for="item of itemLists" :key="item">{{ item.title }}</h3>
+      <!-- <h3>{{ itemLists }}</h3> -->
+    </div>
+  </div>
+</template>
+
+<script setup>
+const itemLists = useState('itemLists', () => [
+  { id: 1, title: 'First Todo' },
+  { id: 2, title: 'Second Todo' },
+]);
+</script>
