57b08b73d406c20d44ef9379f425ea47f0f91eba
rakib <rhkhan9618@gmail.com>
2023-07-19 23:24:50 +0600

    implement font-awesome icon

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  css: ['@/styles/main.css'],
+  css: ['@/styles/main.css', '@fortawesome/fontawesome-svg-core/styles.css'],
   postcss: {
     plugins: {
       tailwindcss: {},
--- pages/index.vue
+++ pages/index.vue
@@ -1,16 +1,30 @@
-<template>
-  <div class="max-w-[400px] mx-auto">
-    <h1 class="text-center text-4xl font-medium py-10">Todo List</h1>
-    <div class="cls">
-      <h3 v-for="item of itemLists" :key="item">{{ item.title }}</h3>
-      <!-- <h3>{{ itemLists }}</h3> -->
-    </div>
-  </div>
-</template>
-
 <script setup>
 const itemLists = useState('itemLists', () => [
   { id: 1, title: 'First Todo' },
   { id: 2, title: 'Second Todo' },
 ]);
+
+function greet(event) {
+  alert(`First Todo'`);
+  // `event` is the native DOM event
+  if (event) {
+    alert(event.target.tagName);
+  }
+}
 </script>
+
+<template>
+  <div class="max-w-[400px] mx-auto">
+    <h1 class="py-10 text-4xl font-medium text-center">Todo List</h1>
+    <div class="cls">
+      <h3
+        v-for="item of itemLists"
+        :key="item"
+        class="bg-[#423a6f] py-3 px-5 border border-purple-800 flex items-center justify-between"
+      >
+        {{ item.title }}
+        <font-awesome-icon icon="fa-solid fa-trash" @click="greet" class="cursor-pointer"/>
+      </h3>
+    </div>
+  </div>
+</template>
