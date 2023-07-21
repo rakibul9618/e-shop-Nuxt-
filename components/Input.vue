f703bcb8f2a77da31b6ddb72b1efe61221292187
rakib <rhkhan9618@gmail.com>
2023-07-21 23:34:48 +0600

    reusable input field created completed

---
+++ components/Input.vue
@@ -0,0 +1,24 @@
+<script setup>
+defineProps({
+  label: {
+    type: [String, Boolean],
+    default: false,
+  },
+  modelValue: {
+    type: String,
+    default: '',
+  },
+});
+</script>
+
+<template>
+  <div class="input-wrap">
+    <label v-if="label">{{ label }}</label>
+    <input
+      type="text"
+      :value="modelValue"
+      @input="$emit('update:modelValue', $event.target.value)"
+      v-bind="$attrs"
+    />
+  </div>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,4 +1,6 @@
 <script setup>
+import Input from '../components/Input.vue';
+const inputValue = useState('inputValue', '');
 const itemLists = useState('itemLists', () => [
   { id: 1, title: 'First Todo' },
   { id: 2, title: 'Second Todo' },
@@ -11,19 +13,27 @@ function greet(event) {
     alert(event.target.tagName);
   }
 }
+
+console.log({ inputValue });
 </script>
 
 <template>
   <div class="max-w-[400px] mx-auto">
     <h1 class="py-10 text-4xl font-medium text-center">Todo List</h1>
-    <div class="cls">
+    <p>{{ inputValue }}</p>
+    <Input v-model="inputValue" class="text-black" />
+    <div class="mt-6">
       <h3
         v-for="item of itemLists"
         :key="item"
         class="bg-[#423a6f] py-3 px-5 border border-purple-800 flex items-center justify-between"
       >
         {{ item.title }}
-        <font-awesome-icon icon="fa-solid fa-trash" @click="greet" class="cursor-pointer"/>
+        <font-awesome-icon
+          icon="fa-solid fa-trash"
+          @click="greet"
+          class="cursor-pointer"
+        />
       </h3>
     </div>
   </div>
